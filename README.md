1fdd4ff1b9c2ed9973055fa82bd23905a7a7eb3e
wizyoung <happyyanghehe@gmail.com>
2018-05-29 11:21:22 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # Minimal-RNN-TensorFlow
-Implementation of Minimal RNN
+This is the TensorFlow implementation of the paper: [MinimalRNN: Toward More Interpretable and Trainable Recurrent Neural Networks](https://arxiv.org/abs/1711.06788) by Minmin Chen in NIPS 2017.
+
+![](https://github.com/wizyoung/Minimal-RNN-TensorFlow/blob/master/rnn_img.png)
+
+### Usage
+
+The usage is quite simple as the API of the Minimal RNN layer is totally the same with other RNN layers (like LSTM, GRU): Just `from rnn_cell import MinimalRNNCell` and use the standard TensorFlow RNN layer API.
+
+An example code (Multi RNN example):
+
+```python
+import tensorflow as tf
+from rnn_cell import MinimalRNNCell
+
+# input_shape: [batch_size, seq_length, feat_dim]
+input = tf.placeholder(tf.float32, [160, 100, 1024], name='inputs')
+
+def get_rnn_cell():
+    return MinimalRNNCell(num_units=128, kernel_initializer=tf.orthogonal_initializer())
+
+multi_rnn_cell_video = tf.contrib.rnn.MultiRNNCell([get_rnn_cell() for _ in range(2)], state_is_tuple=True)
+initial_state = multi_rnn_cell_video.zero_state(batch_size=160, dtype=tf.float32)
+
+rnn_outputs, state = tf.nn.dynamic_rnn(
+    cell=multi_rnn_cell_video,
+    inputs=input,
+    initial_state=initial_state,
+    dtype=tf.float32
+)
+
+print(rnn_outputs)
+print(state)
+```
+
+output:
+
+```
+Tensor("rnn/transpose_1:0", shape=(160, 100, 128), dtype=float32)
+(<tf.Tensor 'rnn/while/Exit_3:0' shape=(160, 128) dtype=float32>, <tf.Tensor 'rnn/while/Exit_4:0' shape=(160, 128) dtype=float32>)
+```
+
+So the usage is totally the same with other RNN layers like GRU!
+
+### NOTE
+
+The RNN layer cells (including LSTM, GRU) in TensorFlow are defined in [tensorflow/python/ops/rnn_cell_impl.py](https://github.com/tensorflow/tensorflow/blob/master/tensorflow/python/ops/rnn_cell_impl.py). The Minimal RNN layer in this repo is inherited from the RNNCell in that file to have the consistent API. Note that the API of the RNN layer cells in TensorFlow has changed a lot after version 1.4, so I implement two versions of Minimal RNN layers corresponding to TensorFlow version <=1.4 and TensorFlow version > 1.4 for compatibility. And the version switch is performed automatically so you don't need to worry about that.
+
---
+++ rnn_cell.py
@@ -0,0 +1,178 @@
+# coding: utf-8
+
+import tensorflow as tf
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import init_ops
+from tensorflow.python.ops import nn_ops
+from tensorflow.python.ops import array_ops
+from tensorflow.python.layers import base as base_layer
+from tensorflow.python.ops import variable_scope as vs
+
+from tensorflow.python.ops.rnn_cell import RNNCell
+
+_BIAS_VARIABLE_NAME = "bias"
+_WEIGHTS_VARIABLE_NAME = "kernel"
+
+__all__ = ['MinimalRNNCell']
+
+tf_version = tf.__version__
+
+def low_tf_version(detect_version, target_version='1.4.0'):
+    detect_version_lst = [int(i) for i in detect_version.split('.')]
+    target_version_lst = [int(i) for i in target_version.split('.')]
+    if ((detect_version_lst[0] > target_version_lst[0]) or
+            ((detect_version_lst[0] == target_version_lst[0]) and (detect_version_lst[1] > target_version_lst[1]))):
+        print('**NOTE**: Your tensorflow version is {}. Now using MinimalRNNCell_new version.'.format(detect_version))
+        return False
+    else:
+        print('**NOTE**: Your tensorflow version is {}. Now using MinimalRNNCell_old version.'.format(detect_version))
+        return True
+
+if low_tf_version(tf_version):
+    from tensorflow.python.ops.rnn_cell_impl import _Linear
+
+    class MinimalRNNCell_old(RNNCell):
+        """MinimalRNN.
+            This implementation is based on:
+            Minmin Chen,
+            "MinimalRNN: Toward More Interpretable and
+            Trainable Recurrent Neural Networks,"
+            https://arxiv.org/abs/1711.06788.
+        """
+
+        def __init__(self,
+                    num_units,
+                    activation=None,
+                    reuse=None,
+                    kernel_initializer=None,
+                    bias_initializer=None):
+            super(MinimalRNNCell_old, self).__init__(_reuse=reuse)
+            self._num_units = num_units
+            self._activation = activation or math_ops.tanh
+            self._kernel_initializer = kernel_initializer
+            self._bias_initializer = bias_initializer
+
+        @property
+        def state_size(self):
+            return self._num_units
+
+        @property
+        def output_size(self):
+            return self._num_units
+
+        def call(self, inputs, state):
+            with vs.variable_scope("map"):
+                self._map_linear = _Linear(
+                    [inputs],
+                    self._num_units,
+                    True,
+                    bias_initializer=self._bias_initializer,
+                    kernel_initializer=self._kernel_initializer)
+
+            z = self._activation(self._map_linear([inputs]))
+            u_inputs = array_ops.concat([z, state], 1)
+
+            with vs.variable_scope("gate"):
+                self._gate_linear = _Linear(
+                    [u_inputs],
+                    self._num_units,
+                    True,
+                    bias_initializer=self._bias_initializer
+                    if self._bias_initializer is not None
+                    else init_ops.constant_initializer(1., dtype=self.dtype),
+                    kernel_initializer=self._kernel_initializer)
+
+            u = math_ops.sigmoid(self._gate_linear([u_inputs]))
+
+            new_state = u * state + (1. - u) * z
+
+            return new_state, new_state
+        
+    MinimalRNNCell = MinimalRNNCell_old
+else:
+    from tensorflow.python.ops.rnn_cell_impl import _LayerRNNCell
+
+    class MinimalRNNCell_new(_LayerRNNCell):
+        """MinimalRNN.
+        This implementation is based on:
+        Minmin Chen,
+        "MinimalRNN: Toward More Interpretable and
+            Trainable Recurrent Neural Networks,"
+        https://arxiv.org/abs/1711.06788.
+        AUTHOR: wizyoung
+        """
+
+        def __init__(self,
+                    num_units,
+                    activation=None,
+                    reuse=None,
+                    kernel_initializer=None,
+                    bias_initializer=None,
+                    name=None):
+            super(MinimalRNNCell_new, self).__init__(_reuse=reuse, name=name)
+
+            # Inputs must be 2-dimensional.
+            self.input_spec = base_layer.InputSpec(ndim=2)
+
+            self._num_units = num_units
+            self._activation = activation or math_ops.tanh
+            self._kernel_initializer = kernel_initializer
+            self._bias_initializer = bias_initializer
+
+        @property
+        def state_size(self):
+            return self._num_units
+
+        @property
+        def output_size(self):
+            return self._num_units
+
+        def build(self, inputs_shape):
+            if inputs_shape[1].value is None:
+                raise ValueError("Expected inputs.shape[-1] to be known, saw shape: %s"
+                                % inputs_shape)
+
+            # input_depth: input dim
+            input_depth = inputs_shape[1].value
+            self._map_kernel = self.add_variable(
+                "map/%s" % _WEIGHTS_VARIABLE_NAME,
+                shape=[input_depth, self._num_units],
+                initializer=self._kernel_initializer)
+            self._map_bias = self.add_variable(
+                "map/%s" % _BIAS_VARIABLE_NAME,
+                shape=[self._num_units],
+                initializer=(
+                    self._bias_initializer
+                    if self._bias_initializer is not None
+                    else init_ops.constant_initializer(0., dtype=self.dtype)))
+            self._gate_kernel = self.add_variable(
+                "gate/%s" % _WEIGHTS_VARIABLE_NAME,
+                shape=[2 * self._num_units, self._num_units],
+                initializer=self._kernel_initializer)
+            self._gate_bias = self.add_variable(
+                "gate/%s" % _BIAS_VARIABLE_NAME,
+                shape=[self._num_units],
+                initializer=(
+                    self._bias_initializer
+                    if self._bias_initializer is not None
+                    else init_ops.constant_initializer(1., dtype=self.dtype)))
+
+            self.built = True
+
+        def call(self, inputs, state):
+
+            z = self._activation(
+                nn_ops.bias_add(math_ops.matmul(inputs, self._map_kernel), self._map_bias))
+
+            u_inputs = array_ops.concat([z, state], 1)
+            u = math_ops.sigmoid(
+                nn_ops.bias_add(math_ops.matmul(
+                    u_inputs, self._gate_kernel), self._gate_bias)
+            )
+
+            new_state = u * state + (1. - u) * z
+
+            return new_state, new_state
+
+
+    MinimalRNNCell = MinimalRNNCell_new
